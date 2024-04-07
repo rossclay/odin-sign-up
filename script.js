f83d26f4d1d02cfb94bf74c4e73ee8107cac41ad
Ross Clay <clay.rossclay@gmail.com>
2024-04-07 12:27:32 -0400

    validates that passwords match

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Validate password
 let password = document.querySelector("#password")
 let confirmPassword = document.querySelector("#confirm-password")
+let passwordSection = document.querySelector('#password-section')
 
 function validatePassword() {
     if (password.value != confirmPassword.value) {
         confirmPassword.setCustomValidity("Passwords Don't Match");
+
     } else {
         confirmPassword.setCustomValidity('');
     }
